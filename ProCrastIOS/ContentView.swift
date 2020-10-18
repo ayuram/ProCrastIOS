351e92223e1b90e6dfa11323ed843e53dfaf7be1
ayuram <rama5398@lgsstudent.org>
2020-10-17 20:16:33 -0700

    first

--- ProCrastIOS/ContentView.swift
+++ ProCrastIOS/ContentView.swift
@@ -1,19 +1,47 @@
 //
 //  ContentView.swift
-//  ProCrastIOS
+//  ProCrast
 //
-//  Created by Ayush Raman on 10/17/20.
+//  Created by Ayush Raman on 8/5/20.
+//  Copyright © 2020 Answer Key. All rights reserved.
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    var curr = view.menu
+    var activities = Activities()
+    var body: some View{
+        TabView{
+            Config()
+                .tabItem{
+                    Image(systemName: "gear")
+                    Text("Settings")
+            }
+            ActivityMenu()
+                .tabItem{
+                    Image(systemName: "list.dash")
+                    Text("Activity List")
+            }
+            Simulation()
+                .tabItem{
+                    Image(systemName: "paperplane")
+                    Text("Dashboard")
+            }
+        }.environmentObject(activities)
     }
+//    func currView() -> some View{
+//        switch curr{
+//        case .menu: return ActivityMenu()
+//        case .simulate: return Simulation()
+//        }
+//    }
 }
-
+enum view{
+    case menu
+    case simulate
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
