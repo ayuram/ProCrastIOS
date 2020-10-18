99cc3f7d3629b697ef221daff19339989435186d
ayuram <rama5398@lgsstudent.org>
2020-10-17 18:08:07 -0700

    Initial Commit

---
+++ ProCrastIOS/ProCrastIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProCrastIOSApp.swift
+//  ProCrastIOS
+//
+//  Created by Ayush Raman on 10/17/20.
+//
+
+import SwiftUI
+
+@main
+struct ProCrastIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
